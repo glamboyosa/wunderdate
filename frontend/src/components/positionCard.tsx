f3aff9859207a90e4f2f3b310f05086d396d5dfc
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-09-09 19:06:38 +0100

    feat: add date and use primary colour in `PositionCard`

--- frontend/src/components/positionCard.tsx
+++ frontend/src/components/positionCard.tsx
@@ -10,10 +10,11 @@ const PositionCard = ({ name, description, count }: TPositionCardProps) => {
       <h2 className="text-xl text-gray-700 mt-1.5 mb-1.5 md:text-3xl ">
         {name}
       </h2>
+      <span>09/10/22</span>
       <p className="text-lg text-gray-600 mt-1.5 mb-1.5  md:text-xl">
         {description}
       </p>
-      <span className="mt-3 text-sm underline text-violet-500 underline-offset-2">
+      <span className="mt-3 text-sm underline text-brightRed underline-offset-2">
         {count} applications &rarr;
       </span>
     </section>
