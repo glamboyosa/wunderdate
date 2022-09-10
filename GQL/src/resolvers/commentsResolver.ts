c3c67fc4d4c28a15e4b8384d5b0be21f15b4d207
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-09-10 20:45:37 +0100

    feat: improved UX and fix TypeGraphQL comment query expected input

--- GQL/src/resolvers/commentsResolver.ts
+++ GQL/src/resolvers/commentsResolver.ts
@@ -1,33 +1,40 @@
 import { Query, Resolver, Mutation, Arg, Ctx } from 'type-graphql'
-import { Comment, CommentCreateInput } from '../generated/typegraphql-prisma'
+import {
+  Comment,
+  CommentCreateInput,
+  CommentCreateWithoutApplicationInput,
+} from '../generated/typegraphql-prisma'
 import { context } from '../utils/types'
 @Resolver()
 export class CommentsResolver {
   @Query(() => [Comment])
   async getCommentsForApplication(
     @Arg('ApplicationId') applicationId: number,
-      @Ctx() { prisma }: context
+    @Ctx() { prisma }: context,
   ) {
-      return await prisma.comment.findMany({
-          where: {
-            applicationId
-        },
+    return await prisma.comment.findMany({
+      where: {
+        applicationId,
+      },
     })
   }
 
   @Mutation(() => Comment)
   async createComment(
-    @Arg('CommentInput') inputData: CommentCreateInput,
+    @Arg('CommentInput') inputData: CommentCreateWithoutApplicationInput,
     @Arg('ApplicationId') applicationId: number,
     @Ctx() { prisma }: context,
   ): Promise<Comment> {
+    console.log(inputData)
     const data = {
       ...inputData,
       application: {
-        connect: [{ id: applicationId }],
+        connect: { id: applicationId },
       },
     }
-    const comment = await prisma.comment.create({ data })
+    const comment = await prisma.comment.create({
+      data,
+    })
     return comment
   }
 }
